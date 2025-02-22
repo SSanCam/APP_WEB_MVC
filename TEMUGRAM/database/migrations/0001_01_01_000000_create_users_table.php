59b39f4e558e5b9393e1ab975fc5bed58b7a4cb6
Camilleri <ssancam391@g.educaand.es>
2025-02-22 12:25:24 +0100

    Capa model

--- TEMUGRAM/database/migrations/0001_01_01_000000_create_users_table.php
+++ TEMUGRAM/database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,11 +12,11 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id(); // ✅ Laravel define esto como autoincremental automáticamente
+            $table->id(); 
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->boolean('banned')->default(false); // ✅ Definir como booleano con default false
+            $table->boolean('banned')->default(false); 
             $table->timestamps();
         });
     }
